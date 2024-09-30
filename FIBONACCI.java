ec8efc0676ed1d418ad79d19c64c92e9fcd19345
YashiSingh21 <cse22172@glbitm.ac.in>
2024-09-30 21:27:11 +0530

    Create FIBONACCI.java

---
+++ FIBONACCI.java
@@ -0,0 +1,10 @@
+class basicRecursion{
+//FIBONACCI
+    public static int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}

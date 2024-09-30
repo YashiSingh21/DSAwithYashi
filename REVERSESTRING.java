e73d96a9bbca546e6073ad4ec248840a3b213b8a
YashiSingh21 <cse22172@glbitm.ac.in>
2024-09-30 21:29:39 +0530

    Create REVERSESTRING.java

---
+++ REVERSESTRING.java
@@ -0,0 +1,10 @@
+class basicRecursion{
+  //REVERSE THE STRING
+    public static String reverseString(String str) {
+        if (str.isEmpty()) {
+            return str;
+        } else {
+            return reverseString(str.substring(1)) + str.charAt(0);
+        }
+    }
+}

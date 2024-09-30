d7c45210e61b9c5b1cfd2395dc8514021214efb1
YashiSingh21 <cse22172@glbitm.ac.in>
2024-09-30 21:31:28 +0530

    Create SUMOFDIGITSOFNUMBER.java

---
+++ SUMOFDIGITSOFNUMBER.java
@@ -0,0 +1,10 @@
+class basicRecursion{
+  //SUM OF DIGITS OF A NUMBER
+    public static int sumOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            return n % 10 + sumOfDigits(n / 10);
+        }
+    }
+}

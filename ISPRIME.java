7256d965264d445051a08f893e2a11ea5c130dd7
YashiSingh21 <cse22172@glbitm.ac.in>
2024-09-30 21:34:59 +0530

    Create ISPRIME.java

---
+++ ISPRIME.java
@@ -0,0 +1,15 @@
+class basicRecursion{
+//FINDING WHETHER THE NUMBER IS PRIME OR NOT
+    public static boolean isPrime(int n, int i) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n % i == 0) {
+            return false;
+        }
+        if (i * i > n) {
+            return true;
+        }
+        return isPrime(n, i + 1);
+    }
+}

f3d1b19d8f271ee8e5d5f23c7a58df3578986a08
YashiSingh21 <cse22172@glbitm.ac.in>
2024-09-30 21:22:59 +0530

    Update and rename BASICRECURSIONINJAVA.java to FACTORIAL.java

---
+++ FACTORIAL.java
@@ -0,0 +1,13 @@
+class basicRecursion {
+//FACTORIAL
+    public static int factorial(int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+}
+
+
+
